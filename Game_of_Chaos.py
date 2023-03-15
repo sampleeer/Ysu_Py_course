bea0b846555537beb9f8785b171478332e3ecd47
Pasha Nekhaenko <86608170+sampleeer@users.noreply.github.com>
2023-03-16 00:56:52 +0300

    fixed pep8
    fixed some spaces for pep8

--- Game_of_Chaos.py
+++ Game_of_Chaos.py
@@ -13,13 +13,14 @@ y3 = -R
 x0 = randint(-299, 299)
 y0 = randint(-299, 299)
 
+
 def start_position(x1, y1, x2, y2, x3, y3):
     global x0
     global y0
     a = (x1 - x0) * (y2 - y1) - (x2 - x1) * (y1 - y0)
     b = (x2 - x0) * (y3 - y2) - (x3 - x2) * (y2 - y0)
     c = (x3 - x0) * (y1 - y3) - (x1 - x3) * (y3 - y0)
-    while not((a >= 0 and b >= 0 and c >= 0) or (a <= 0 and b <= 0 and c <= 0)):
+    while not ((a >= 0 and b >= 0 and c >= 0) or (a <= 0 and b <= 0 and c <= 0)):
         x0 = randint(-299, 299)
         y0 = randint(-299, 299)
         a = (x1 - x0) * (y2 - y1) - (x2 - x1) * (y1 - y0)
@@ -27,13 +28,13 @@ def start_position(x1, y1, x2, y2, x3, y3):
         c = (x3 - x0) * (y1 - y3) - (x1 - x3) * (y3 - y0)
     print(x0, y0)
 
+
 print(start_position(x1, y1, x2, y2, x3, y3))
 
 # Initializing screen
 turtle.TurtleScreen._RUNNING = True
 myScreen = turtle.Screen()
 myScreen.setup(width=1.0, height=1.0)
-
 
 # Initializing pen
 myPen = turtle.Turtle()
@@ -53,9 +54,9 @@ myPen.dot()
 step = 40000
 turtle.speed("fastest")
 turtle.tracer(0, 0)
-while(step):
+while (step):
     turtle.speed(10000000)
-    rand_num = randint(1,3)
+    rand_num = randint(1, 3)
     print(rand_num)
     if rand_num == 1:
         x0 = (x0 + x1) // 2
@@ -71,5 +72,5 @@ while(step):
     step -= 1
 
 turtle.update()
-print("The end!")
+print("The end!!")
 myScreen.exitonclick()
